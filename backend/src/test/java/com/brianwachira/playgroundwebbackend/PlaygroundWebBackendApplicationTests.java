9bff053939024ea92fe51daf18ffa1eee4372558
brianwachira <brianwachira7@gmail.com>
2020-09-19 12:31:44 +0300

    make intial commit

---
+++ backend/src/test/java/com/brianwachira/playgroundwebbackend/PlaygroundWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianwachira.playgroundwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
