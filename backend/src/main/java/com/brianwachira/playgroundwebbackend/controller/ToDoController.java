d9d088279229653d95cd0f7a755ce6347cbc5d0a
brianwachira <brianwachira7@gmail.com>
2020-09-19 20:18:31 +0300

    add endpoints

--- backend/src/main/java/com/brianwachira/playgroundwebbackend/controller/ToDoController.java
+++ backend/src/main/java/com/brianwachira/playgroundwebbackend/controller/ToDoController.java
@@ -1,2 +1,45 @@
-package com.brianwachira.playgroundwebbackend.controller;public class ToDoController {
+package com.brianwachira.playgroundwebbackend.controller;
+
+import com.brianwachira.playgroundwebbackend.data.ToDo;
+import com.brianwachira.playgroundwebbackend.service.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
+
+public class ToDoController {
+
+    @Autowired
+    private ToDoService toDoService;
+
+    @GetMapping
+    public List<ToDo> findAll(){
+        return toDoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ToDo findById(@PathVariable String id){
+        return toDoService.findById(id);
+    }
+
+    @PostMapping
+    public ToDo create(@RequestBody ToDo toDo){
+        return toDoService.save(toDo);
+    }
+
+    @PutMapping("/{id}")
+    public ToDo update(@RequestBody ToDo toDo){
+        return toDoService.save(toDo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id){
+        toDoService.deleteById(id);
+    }
+
+
+
 }
