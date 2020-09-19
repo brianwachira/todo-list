14af31f034bea95b66d65e2c49943c9e7c61b3f5
brianwachira <brianwachira7@gmail.com>
2020-09-19 20:21:47 +0300

    add data repository

--- backend/src/main/java/com/brianwachira/playgroundwebbackend/repository/ToDoRepository.java
+++ backend/src/main/java/com/brianwachira/playgroundwebbackend/repository/ToDoRepository.java
@@ -1,2 +1,8 @@
-package com.brianwachira.playgroundwebbackend.repository;public interface ToDoRepository {
+package com.brianwachira.playgroundwebbackend.repository;
+
+import com.brianwachira.playgroundwebbackend.data.ToDo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ToDoRepository extends MongoRepository<ToDo, String> {
+
 }
